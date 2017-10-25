67eaf844391fcc3291bbc189fa30c12cc2561dfb
“Yarema <Juxx@bk.ru>
2017-10-25 10:43:45 +0300

    router

--- src/component/Validation.js
+++ src/component/Validation.js
@@ -10,40 +10,46 @@ class Validation extends Component{
      this.clickOkay = this.clickOkay.bind(this);
    }
 
-   clickOkay(event){
-     if(event.target.className === "btn btn-danger btn-lg btn-block"){
-       this.props.validation(false);
-       this.props.newStateAddForm(true);
-     }
-     this.props.validation(false);
-     var d = this.props.data.validation;
-     axios.post('/newdoc', {
-       programmNumber:               d.programmNumber,
-       fullName:                     d.fullName,
-       company:                      d.company,
-       personnelCategory:            d.personnelCategory,
-       subdivision:                  d.subdivision,
-       position:                     d.position,
-       theoreticalTraining:          d.theoreticalTraining,
-       practicalTraining:            d.practicalTraining,
-       internship:                   d.internship,
-       termOfTraining:               d.termOfTraining,
-       trainingInTheUTC:             d.trainingInTheUTC,
-       trainingInTheUnit:            d.trainingInTheUnit,
-       registrationDateOfTheProgram: d.registrationDateOfTheProgram,
-       dateOfDelivery:               d.dateOfDelivery,
-       fullNameProgram:              d.fullNameProgram,
-       link:                         d.link,
-       status:                       true
-     })
-     .then(function (response) {
-       console.log(response);
-     })
-     .catch(function (error) {
-       console.log(error);
-     });
 
-    }
+
+   clickOkay(event){
+     console.log('s')
+   }
+
+
+    //  if(event.target.className === "btn btn-danger btn-lg btn-block"){
+    //    this.props.validation(false);
+    //    this.props.newStateAddForm(true);
+    //  }
+    //  this.props.validation(false);
+    //  var d = this.props.data.validation;
+    //  axios.post('/newdoc', {
+    //    programmNumber:               d.programmNumber,
+    //    fullName:                     d.fullName,
+    //    company:                      d.company,
+    //    personnelCategory:            d.personnelCategory,
+    //    subdivision:                  d.subdivision,
+    //    position:                     d.position,
+    //    theoreticalTraining:          d.theoreticalTraining,
+    //    practicalTraining:            d.practicalTraining,
+    //    internship:                   d.internship,
+    //    termOfTraining:               d.termOfTraining,
+    //    trainingInTheUTC:             d.trainingInTheUTC,
+    //    trainingInTheUnit:            d.trainingInTheUnit,
+    //    registrationDateOfTheProgram: d.registrationDateOfTheProgram,
+    //    dateOfDelivery:               d.dateOfDelivery,
+    //    fullNameProgram:              d.fullNameProgram,
+    //    link:                         d.link,
+    //    status:                       true
+    //  })
+    //  .then(function (response) {
+    //    console.log(response);
+    //  })
+    //  .catch(function (error) {
+    //    console.log(error);
+    //  });
+
+    
 
    render(){
 
@@ -67,20 +73,9 @@ class Validation extends Component{
 
 
      //Функция проверки формата даты;
-     var regExp = (data) => {
-       var errorDate = /(0[1-9]|[12][0-9]|3[01])[.](0[1-9]|1[012])[.]\d\d/.test(data);
-       return errorDate;
-     }
 
-     collectionDate.forEach((item, index) => {
-       let test = regExp(item);
-       if(!test){
-        massage = 'Дата введена не коректно (ДД.ММ.ГГГГ)';
-        danger = true;
-       }
-     })
 
-     
+
       return(
          <div className="validation">
             <div className="validationHeader">
